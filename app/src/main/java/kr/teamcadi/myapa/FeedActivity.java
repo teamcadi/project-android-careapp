a0c403776db78bffeba6025fd8068763f699f073
soohyun <soohyun6879@naver.com>
2021-01-16 17:20:45 +0900

    Add: ReplyActivity, Update: 주석 수정

--- app/src/main/java/kr/teamcadi/myapa/FeedActivity.java
+++ app/src/main/java/kr/teamcadi/myapa/FeedActivity.java
@@ -9,24 +9,24 @@ import android.view.MenuItem;
 // 화면 설명 : 피드 화면
 // Author : Soohyun, Last Modified : 2020.01.15
 public class FeedActivity extends AppCompatActivity {
-    Toolbar toolbar;
+    Toolbar toolbar; // 상단바
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        // toolbar 설정
+        // 상단바 설정
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //기본 타이틀 삭제
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //back버튼
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case android.R.id.home:{ //toolbar의 back키 눌렀을 때
+            case android.R.id.home:{ //상단바의 back키 눌렀을 때
                 finish();
                 return true;
             }
--- app/src/main/java/kr/teamcadi/myapa/FindIdAndPwActivity.java
+++ app/src/main/java/kr/teamcadi/myapa/FindIdAndPwActivity.java
@@ -14,20 +14,20 @@ import com.google.android.material.tabs.TabLayout;
 // 화면 설명 : 아이디/비밀번호 찾기 화면
 // Author : Soohyun, Last Modified : 2020.12.31
 public class FindIdAndPwActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    TabLayout tabLayout;
-    View line;
-    EditText ev_phoneNumber;
-    Button btn_send_certificationNumber;
-    EditText ev_certificationNumber;
-    Button btn_indentify_certifiationNumber;
+    Toolbar toolbar; // 상단바
+    TabLayout tabLayout; // 탭
+    View line; // 구분선
+    EditText ev_phoneNumber; // 전화번호 입력창
+    Button btn_send_certificationNumber; // 인증번호 전송 버튼
+    EditText ev_certificationNumber; // 인증번호 입력창
+    Button btn_indentify_certifiationNumber; // 인증번호 확인 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_id_and_pw);
 
-        // toolbar 설정
+        // 상단바 설정
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false); //기본 타이틀 삭제
@@ -39,6 +39,7 @@ public class FindIdAndPwActivity extends AppCompatActivity {
         ev_certificationNumber = (EditText)findViewById(R.id.ev_certificationNumber);
         btn_indentify_certifiationNumber = (Button)findViewById(R.id.btn_indentify_certificationNumber);
 
+        // 첫화면(로그인찾기화면)일 때 안보이도록 하는 부분
         line.setVisibility(View.INVISIBLE);
         ev_phoneNumber.setVisibility(View.INVISIBLE);
         btn_send_certificationNumber.setVisibility(View.INVISIBLE);
@@ -51,7 +52,7 @@ public class FindIdAndPwActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 int pos = tab.getPosition();
-                changeTab(pos);
+                changeTab(pos); // 탭 전환
             }
 
             @Override
@@ -70,6 +71,7 @@ public class FindIdAndPwActivity extends AppCompatActivity {
     private void changeTab(int pos) {
         switch (pos) {
             case 0:
+                // 아이디 찾기 화면일 때 안보이도록 변경
                 line.setVisibility(View.INVISIBLE);
                 ev_phoneNumber.setVisibility(View.INVISIBLE);
                 btn_send_certificationNumber.setVisibility(View.INVISIBLE);
@@ -77,6 +79,7 @@ public class FindIdAndPwActivity extends AppCompatActivity {
                 btn_indentify_certifiationNumber.setVisibility(View.INVISIBLE);
                 break;
             case 1:
+                // 비밀번호 찾기 화면일 때 보이도록 변경
                 line.setVisibility(View.VISIBLE);
                 ev_phoneNumber.setVisibility(View.VISIBLE);
                 btn_send_certificationNumber.setVisibility(View.VISIBLE);
@@ -89,7 +92,7 @@ public class FindIdAndPwActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case android.R.id.home:{ //toolbar의 back키 눌렀을 때
+            case android.R.id.home:{ //상단바의 back키 눌렀을 때
                 finish();
                 return true;
             }
