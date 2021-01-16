c99b9046e779f85cdd2471babb16650427b0146d
raae7742 <aeae_1@naver.com>
2021-01-17 00:27:48 +0900

    Add: splash, login resource file

---
+++ app/src/main/java/kr/teamcadi/myapa/MemberJoinDetail.java
@@ -0,0 +1,16 @@
+package kr.teamcadi.myapa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// 화면 설명 : 회원가입 세부 정보 입력 화면
+// Author : Kim Seung Hyun, Last Modified : 2020.11.21
+public class MemberJoinDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_join_detail);
+    }
+}
