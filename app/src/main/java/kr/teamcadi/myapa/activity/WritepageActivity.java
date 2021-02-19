7b95661fd7aacfb9b1b5c856b5c18f39df41e50e
raae7742 <aeae_1@naver.com>
2021-02-20 01:29:36 +0900

    Update : 마이페이지 화면 연결, 사진 추가 버튼 적용

--- app/src/main/java/kr/teamcadi/myapa/activity/WritepageActivity.java
+++ app/src/main/java/kr/teamcadi/myapa/activity/WritepageActivity.java
@@ -1,25 +1,44 @@
 package kr.teamcadi.myapa.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.InputStream;
 
 import kr.teamcadi.myapa.R;
 
 // 화면 설명 : 글쓰기 화면 -> 홈 화면의 글 추가 버튼과 연결
-// Author : Soohyun, Last Modified : 2021.02.06
+// Author : Hyeonae, Last Modified : 2021.02.20
 public class WritepageActivity extends AppCompatActivity {
+    private static final int GET_FROM_GALLERY = 101;        // 사진 불러오기 코드
+
+    ImageView iv;
+
     Toolbar toolbar; // 상단바
     Spinner wp_spinner; // 진료과 선택 스피너
+    ImageButton ib_camera;  // 카메라(사진 추가) 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +51,8 @@ public class WritepageActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false); // 기본 타이틀 삭제
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //back버튼
 
+        ib_camera = (ImageButton)findViewById(R.id.ib_camera);  // 카메라 버튼
+
         wp_spinner = findViewById(R.id.wp_spinner);
 
         String[] medical = {"안과", "내과", "이비인후과", "소아과", "치과", "산부인과", "비뇨기과", "암", "외과", "피부과"}; // 진료과 선택 스피너에 넣을 진료과 내용
@@ -39,6 +60,15 @@ public class WritepageActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item); // 스피너 스타일
         wp_spinner.setAdapter(adapter); // 스피너와 어댑터 연결
 
+        ib_camera.setOnClickListener(new View.OnClickListener() {   // 카메라 버튼의 OnClickListener
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();                            // 갤러리에서 사진 불러오기
+                i.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(i, GET_FROM_GALLERY);
+            }
+        });
     }
 
     // 상단바 오른쪽 메뉴
@@ -61,5 +91,24 @@ public class WritepageActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 101 && resultCode == RESULT_OK) { // 카메라 버튼을 눌렀을 때
+            try {
+                InputStream is = getContentResolver().openInputStream(data.getData());
+                Bitmap bm = BitmapFactory.decodeStream(is);
+                is.close();
+                iv.setImageBitmap(bm);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else if(requestCode == 101 && resultCode == RESULT_CANCELED){
+
+        }
     }
 }
--- app/src/main/java/kr/teamcadi/myapa/fragment/MyPageFragment.java
+++ app/src/main/java/kr/teamcadi/myapa/fragment/MyPageFragment.java
@@ -1,15 +1,20 @@
 package kr.teamcadi.myapa.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 import kr.teamcadi.myapa.R;
+import kr.teamcadi.myapa.activity.ReportActivity;
 
 // 화면 설명 : 마이 페이지 화면 -> MainActivity로 연결
 // Author : Jaey, Last Modified : 2021.01.16
